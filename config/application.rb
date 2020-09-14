1a75e070691acc46bf5fb5926ffb1d7216c3e366
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-14 11:10:34 +0900

    rails g実行時にassets、helper、jbuilderを自動作成しない設定

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,10 @@ Bundler.require(*Rails.groups)
 module TaskManagement
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
