4bb8e2974d0a66d9007f31591886e729fddaaf0f
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-14 21:33:31 +0900

    add: タスクを削除できるようにする

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i(show edit update)
+  before_action :set_task, only: %i(show edit update destroy)
 
   def index
     @tasks = Task.all
@@ -30,6 +30,11 @@ class TasksController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: %q(タスクを削除しました。)
   end
 
   private
