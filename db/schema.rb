1d44a10bc6f46474018f3118afb1da6af62baadd
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-14 14:25:02 +0900

    add: tasksテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_032611) do
+ActiveRecord::Schema.define(version: 2020_09_14_051659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2020_09_14_032611) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline", null: false
+    t.integer "priority", null: false
+    t.integer "status", null: false
   end
 
 end
