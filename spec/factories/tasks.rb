34403a812e2cb32d6993003b37532d0c6a6d0c60
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-16 07:00:17 +0900

    System Specをタスク機能に対して書く

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,11 +1,6 @@
 FactoryBot.define do
-  factory :task do
-    title { 'Factoryで作ったデフォルトのタイトル１' }
-    content { 'Factoryで作ったデフォルトのコンテント１' }
-  end
-
-  factory :second_task, class: Task do
-    title { 'Factoryで作ったデフォルトのタイトル２' }
-    content { 'Factoryで作ったデフォルトのコンテント２' }
+  factory :testtask, class: Task do
+    title { 'Faker::Name.first_name' }
+    content { 'Faker::Name.name' }
   end
 end
