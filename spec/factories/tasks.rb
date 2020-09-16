1549c24a1a409ce9329e75ce6adf9a35c334a5c4
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-16 22:31:10 +0900

    add: タスク一覧を作成日時の順番で並び替え、テスト実施

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :testtask, class: Task do
-    title { 'タスク１' }
-    content { 'タスク１内容' }
+    title { 'factory_title1' }
+    content { 'factory_content1' }
+  end
+  factory :testtask_second, class: Task do
+    title { 'factory_title2' }
+    content { 'factory_content2' }
   end
 end
--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    before do
+      FactoryBot.create(:testtask)
+      new_task = FactoryBot.create(:testtask_second)
+      visit tasks_path
+    end
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:testtask)
-        visit tasks_path
-        expect(page).to have_content 'タスク１'
-        expect(page).to have_content 'タスク１内容'
+        expect(page).to have_content 'factory_title1'
+        expect(page).to have_content 'factory_title2'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'factory_title2'
+        expect(task_list[1]).to have_content 'factory_title1'
       end
     end
   end
@@ -15,9 +25,9 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        fill_in 'Title', with: 'systemspec'
-        fill_in 'Content', with: 'createtest'
-        click_on 'Create Task'
+        fill_in 'タスク名', with: 'systemspec'
+        fill_in 'タスク詳細', with: 'createtest'
+        click_on '登録する'
         expect(page).to have_content 'systemspec'
         expect(page).to have_content 'createtest'
       end
@@ -27,10 +37,10 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
         it '該当タスクの内容が表示される' do
-          task = FactoryBot.create(:testtask, content: 'タスク１追加内容')
+          task = FactoryBot.create(:testtask)
           visit task_path(task.id)
-          expect(page).to have_content 'タスク１'
-          expect(page).to have_content 'タスク１追加内容'
+          expect(page).to have_content 'factory_title1'
+          expect(page).to have_content 'factory_content1'
        end
      end
   end
