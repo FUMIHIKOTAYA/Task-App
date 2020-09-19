92d3cb802172db125156b7f95d0520dbf8413a26
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-19 10:10:35 +0900

    add: 終了期限でソートする機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,11 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i(show edit update destroy)
 
   def index
-    @tasks = Task.all.order(created_at: :DESC)
+    if params[:sort_expired]
+      @tasks = Task.all.order(deadline: :DESC)
+    else
+      @tasks = Task.all.order(created_at: :DESC)
+    end
   end
 
   def new
@@ -40,7 +44,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :deadline)
   end
 
   def set_task
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_115404) do
+ActiveRecord::Schema.define(version: 2020_09_18_233223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_115404) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline", default: "2020-09-30 15:00:00", null: false
   end
 
 end
