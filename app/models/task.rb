a23dd3a963fc759104f230ee8dc9c9654531a826
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-19 17:47:33 +0900

    add: タスクにステータスが登録できるよう設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  enum status: {
+    waiting: 0,
+    working: 1,
+    done: 2
+  }
 end
