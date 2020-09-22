d0e3f28ceeb71192a59c5848e1caea98a9a2bfc2
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-22 18:25:39 +0900

    add: System Specを書く(ステータス登録・検索を追加)

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,10 +3,18 @@ FactoryBot.define do
     title { 'factory_title1' }
     content { 'factory_content1' }
     deadline { Time.current }
+    status { 2 }
   end
   factory :testtask_second, class: Task do
     title { 'factory_title2' }
     content { 'factory_content2' }
+    deadline { Time.current.next_month }
+    status { 0 }
+  end
+  factory :testtask_third, class: Task do
+    title { 'factory_title3' }
+    content { 'factory_content3' }
     deadline { Time.current.tomorrow }
+    status { 1 }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   let!(:task) { FactoryBot.create(:testtask) }
-  let!(:new_task) { FactoryBot.create(:testtask_second) }
+  let!(:second_task) { FactoryBot.create(:testtask_second) }
+  let!(:third_task) { FactoryBot.create(:testtask_third) }
   before do
     visit tasks_path
   end
@@ -16,8 +17,9 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
         task_list = all('.task_row')
-        expect(task_list[0]).to have_content 'factory_title2'
-        expect(task_list[1]).to have_content 'factory_title1'
+        expect(task_list[0]).to have_content 'factory_title3'
+        expect(task_list[1]).to have_content 'factory_title2'
+        expect(task_list[2]).to have_content 'factory_title1'
       end
     end
     context '終了期限でソートするというリンクを押した場合' do
@@ -26,7 +28,8 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'タスク一覧'
         task_list = all('.task_row')
         expect(task_list[0]).to have_content 'factory_title2'
-        expect(task_list[1]).to have_content 'factory_title1'
+        expect(task_list[1]).to have_content 'factory_title3'
+        expect(task_list[2]).to have_content 'factory_title1'
       end
     end
   end
@@ -39,10 +42,12 @@ RSpec.describe 'タスク管理機能', type: :system do
         fill_in 'タスク詳細', with: 'content_sample'
         select_date("2020,9,29", from: "終了期限")
         select_time("17", "00", from: "終了期限")
+        select '着手中', from: 'ステータス'
         click_on '登録する'
         expect(page).to have_content 'title_sample'
         expect(page).to have_content 'content_sample'
         expect(page).to have_content '2020/09/29 17:00'
+        expect(page).to have_content '着手中'
       end
     end
   end
@@ -55,5 +60,30 @@ RSpec.describe 'タスク管理機能', type: :system do
           expect(page).to have_content 'factory_content1'
        end
      end
+  end
+
+  describe '検索機能' do
+    context 'タイトルであいまい検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        fill_in 'タスク名', with: '1'
+        click_on 'searchbutton'
+        expect(page).to have_content 'factory_title1'
+      end
+    end
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        select '未着手', from: 'status'
+        click_on 'searchbutton'
+        expect(page).to have_content 'factory_title2'
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        fill_in 'タスク名', with: 'factory'
+        select '着手中', from: 'status'
+        click_on 'searchbutton'
+        expect(page).to have_content 'factory_title3'
+      end
+    end
   end
 end
