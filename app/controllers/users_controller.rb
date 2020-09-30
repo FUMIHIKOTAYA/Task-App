960cba5ea4ddaee585e6fc63c522e990420a5688
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-10-01 07:14:43 +0900

    add: ログインユーザーのアクセスを制御する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_user, only: %i(new)
+  before_action :another_account, only: %i(show)
+
   def new
     @user = User.new
   end
@@ -12,13 +15,24 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def logged_user
+    if logged_in?
+      redirect_to user_path(current_user.id), notice: %q(ログイン済みです。)
+    end
+  end
+
+  def another_account
+    @user = User.find(params[:id])
+    unless params[:id].to_i == current_user.id
+      redirect_to tasks_path, notice: %q(実行権限がありません。)
+    end
   end
 end
