5e3b6793eccd44d4ed46eaeb2e882a8d96d90408
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-10-05 07:45:56 +0900

    add: ロールの付与と削除ができる、モデルのコールバックを利用して更新・削除の制御

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
+  before_update :prevent_admin_zero
+  before_destroy :prevent_admin_disappear
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   validates :name, presence: true, length: { maximum: 30 }
@@ -8,4 +11,18 @@ class User < ApplicationRecord
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
     uniqueness: true
   before_validation { email.downcase! }
+
+  def prevent_admin_zero
+    if User.where(admin: true).count == 1 && self.admin == false
+      throw(:abort)
+    end
+  end
+
+  def prevent_admin_disappear
+    if User.where(admin: true).count <= 1 && self.admin == true
+      throw(:abort)
+    end
+  end
+
+
 end
