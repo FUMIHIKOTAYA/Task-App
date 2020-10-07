2270e86f64c30e7c2ff572ed6dacaf171b7b8d99
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-10-08 07:28:11 +0900

    add: テスト項目（ユーザー登録・セッション機能・管理画面）を満たすようSystem Specを書く

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :administrator, class: User do
+    name { 'factory_name1' }
+    email { 'admin@factory.com' }
+    password { 'aaaaaa' }
+    admin { 'true' }
+  end
+  factory :user, class: User do
+    name { 'factory_name2' }
+    email { 'user@factory.com' }
+    password { 'bbbbbb' }
+    admin { 'false' }
   end
 end
