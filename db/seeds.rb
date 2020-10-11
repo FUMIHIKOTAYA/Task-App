b6bae9f136082547c37b54a12bc2f7565cd0f054
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-10-12 07:21:35 +0900

    Fix: タスクとユーザーについてのSeedデータを用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,25 @@
 User.create(name: "firstperson", email: "first@example.com", password: "ufirst", password_confirmation: "ufirst")
+
+10.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+                   email: email,
+                   password: password,
+                   password_confirmation: password,
+                   )
+end
+
+task_list = []
+10.times do |n|
+  task_list <<
+    { title: Faker::Book.title,
+      content: Faker::Quote.famous_last_words,
+      deadline: Faker::Date.between(from: Date.today, to: 3.days.since),
+      status: 1,
+      priority: 1,
+      user_id: User.ids.sample,
+    }
+end
+Task.create!(task_list)
