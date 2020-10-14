0161507034d2f18864b6060d1661c91aec77371f
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-10-14 15:00:26 +0900

    Fix: 管理者としてログインしつつ、新規ユーザーを作成できるように設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,7 +13,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_user_path(@user.id)
+      redirect_to admin_users_path, notice: t('view.flash.admin_user_new')
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to admin_user_path, notice: t('view.flash.admin_user_update')
+      redirect_to admin_users_path, notice: t('view.flash.admin_user_update')
     else
       render :edit
     end
